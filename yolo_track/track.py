17a1def231d9d4e9723f69acf7ee56b9fc03fb57
redthing1 <redthing1@alt.icu>
2022-03-10 19:21:04 -0800

    clean up runs dir

--- yolo_track/track.py
+++ yolo_track/track.py
@@ -263,7 +263,7 @@ def main():
     parser.add_argument('--visualize', action='store_true', help='visualize features')
     parser.add_argument('--max-det', type=int, default=1000, help='maximum detection per image')
     parser.add_argument('--dnn', action='store_true', help='use OpenCV DNN for ONNX inference')
-    parser.add_argument('--project', default=ROOT / 'runs/track', help='save results to project/name')
+    parser.add_argument('--project', default='runs/track', help='save results to project/name')
     parser.add_argument('--name', default='exp', help='save results to project/name')
     parser.add_argument('--out-txt', default=None, help='directly override output txt file')
     parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
